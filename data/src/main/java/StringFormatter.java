29153c3ed4fcdd5f8a5ceb442bec2eecf345bf68
AlinaBlago <alinablago19@gmail.com>
2020-07-17 23:58:29 +0300

    Task one complited.

---
+++ data/src/main/java/StringFormatter.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StringFormatter {
+    public static int format(List<String> string){
+        AtomicInteger number = new AtomicInteger();
+        string.stream().forEach(currentStr -> {
+            currentStr.chars().
+                    filter(charCanBeDigit -> Character.isDigit(charCanBeDigit)).forEach((str)->{
+                number.updateAndGet(v -> v * 10);
+                number.updateAndGet(v -> v + Integer.parseInt(Character.toString(str)));
+            });
+        });
+
+        System.out.println("Введенная строка: " + string + "\nПолученная строка цифр: " + number.get());
+
+        return number.get();
+
+    }
+}
--- mainModule/src/main/java/Main.java
+++ mainModule/src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Task 1");
+        System.out.println("_____________________________");
+        List<String> strings = Arrays.asList("jjhbdx1", "2jghfj", "dd3vggf4");
+        StringFormatter.format(strings);
     }
 }
